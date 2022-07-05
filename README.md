541b8e728535ae16de8898434c17fc288ecceaff
arifrhm <arif.rahman.rhm@gmail.com>
2022-07-05 23:24:06 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Alert Handler Test
+
+This is repo for testing alert handler component of the website
+
+We can start from start.sh to configure virtual environment and dependency we need to do test of alert handler.
+
+We need to change start.sh file as executable file in bash command terminal with this command:
+```
+sudo chmod +x start.sh
+```
+We can start using start.sh by using command :
+```
+./start.sh
+```
+
+So we can start to use alert handler in main.py file with command :
+```
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+
+def test_website():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    url = "https://demoqa.com/alerts/"
+    driver.get (url)
+    time.sleep(2)
+    entry_content =  driver.find_element(By.ID, 'alertButton')
+    print(entry_content.accessible_name)
+    entry_content.click()
+    time.sleep(30)
+    
+if __name__ =="__main__" :
+    test_website()
